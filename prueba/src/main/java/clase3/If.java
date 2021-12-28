a83272a2434e9b21d9fcbfa227c6ba448e31a2cd
User <User@Nicolas-Quintero>
2021-12-28 10:37:16 -0500

    Ejercicio Clase

--- prueba/src/main/java/clase3/If.java
+++ prueba/src/main/java/clase3/If.java
@@ -5,20 +5,59 @@ import java.util.Scanner;
 public class If {
 
 	public static void main(String[] args) {
-		
-	String usuario;
-	String contrasenia; 
-	
-	usuario = "jose";
-	contrasenia = "12345";
-	
-	if (usuario == "admin" && contrasenia == "1234") {
-		System.out.println("ok");
-	
-		} else {
-			System.out.println("fail");
+
+
+		// SOLICITANDO LA CANTIDAD DE ALUMNOS A PROCESAR
+
+		//Abrimos el teclado
+		Scanner teclado = new Scanner(System.in);
+
+		// pedimos la cantidad de alumnos y la guardamos en la variable cantidadAlumnos
+		System.out.println("Por favor ingrese la cantidad de alumnos a procesar: ");
+
+		int cantidadAlumnos = teclado.nextInt();
+
+		// PARA CADA ALUMNO SE DEBERAN INGRESAR NOMBRE, APELLIDO, NUMERO DE EXAMEN Y NOTA
+
+		// declaramos el vector para los estudiantes y los datos, donde cada fila es un estudiante
+		// pos 0: estudiante 1, pos 1: estudiante 2,...
+		// y las columnas pos 0: numbre, pos 1: apellido, pos 2: numero de examen, pos3: nota, pos 4: nota 2...
+
+		// primero preguntamos cuantos examenes hubo en el periodo
+		System.out.println("Por favor ingrese el número de exámenes hechos durante el periodo: ");
+		int numExamenes = teclado.nextInt();
+
+		// luego obtenemos el numero de columnas
+		int cantidadColumnas = (numExamenes*2)+2;
+
+		Object [][] datosAlumnos = new Object [cantidadAlumnos][cantidadColumnas];
+
+		// Pedimos los datos y llenamos el vector con estos
+		for (int i = 0; i<cantidadAlumnos; i++) {
+		for (int j = 0; j < cantidadColumnas; j++) {
+		if (j == 0) {
+		System.out.println("Por favor ingrese el nombre del estudiante: ");
+		datosAlumnos[i][j] = teclado.next();
 		}
-	
+		if (j == 1) {
+		System.out.println("Por favor ingrese el apellido del estudiante: ");
+		datosAlumnos[i][j] = teclado.next();
+		}
+		if (j % 2 == 0 && j != 0) {
+		System.out.println("Por favor ingrese el numero del examen: ");
+		
+		
+		datosAlumnos[i][j] = teclado.nextInt();
+		}
+		if (j % 2 != 0 && j != 1) {
+		System.out.println("Por favor ingrese la nota del examen (entre 0 y 10 inclusive)");
+		datosAlumnos[i][j] = teclado.nextDouble();
+		}
+		}
+		}
+
+		// cerramos el teclado
+		teclado.close();
+		
 	}
 }
-
--- prueba/src/main/java/tp/Ejercicio2.java
+++ prueba/src/main/java/tp/Ejercicio2.java
@@ -2,59 +2,132 @@ package tp;
 
 import java.util.Scanner;
 
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 	
-		String nombre = new String();
-		String apellido = new String();
-		String NumExamen = new String();
-		double cantidad, nota, promedio;
+
 		Scanner teclado = new Scanner(System.in);
+
 		System.out.println("Ingrese la cantidad de estudiantes: ");
-		
-		int TotEst = teclado.nextInt();
+		int TotEst = teclado.nextInt();	
 		while (TotEst<0) {
+
 			System.out.println("La cantidad no es válida.");
 			System.out.println("Ingrese la cantidad de estudiantes: ");
-				TotEst = teclado.nextInt();
+			TotEst = teclado.nextInt();
 		}
-		System.out.println("Ingresó "+TotEst+" estudiantes.");
-				
-		String[] nombres = new String[TotEst];
-		String[] apellidos = new String[TotEst];
-		
-		
-		for(int i=0;i < nombres.length ; i++) {
-			System.out.println("Ingrese nombre del estudiante número "+(i+1));
-			nombres[i] = teclado.next();
-			System.out.println("Ingrese apellido del estudiante número "+(i+1));
-			apellidos[i] = teclado.next(); 
-			
-		}
-		
-		
-	
-		System.out.println("Ingrese la cantidad de examenes: ");
-		int TotExm=teclado.nextInt();
-		
+
+		System.out.println("Ingrese la cantidad de exámenes realizados: ");
+		int TotExm = teclado.nextInt();
 		while (TotExm<0) {
+
 			System.out.println("La cantidad no es válida.");
-			System.out.println("Ingrese la cantidad de examenes: ");
-				TotExm = teclado.nextInt();
+			System.out.println("Ingrese la cantidad de exámenes realizados: ");
+			TotExm = teclado.nextInt();
 		}
-			
-			double[] notas = new double[TotExm];
-						
-	
-			for (int i=0;i <TotExm; i++) {
-				System.out.println("Ingrese la calificacion del examen " +(i+1) +" del estudiante "+nombres[i]+" "+apellidos[i]);
-				notas[i]=teclado.nextDouble();
+
+
+		int CantCol = (TotExm+3);
+
+
+		Object [][] DatosEst = new Object [TotEst][CantCol];
+
+		for (int i = 0; i<TotEst; i++) {
+			for (int j = 0; j < CantCol-1; j++) {
+				if (j == 0) {
+					System.out.println("Ingrese el nombre del estudiante número "+(i+1)+": ");
+					DatosEst[i][j] = teclado.next();
+				}
+				if (j == 1) {
+					System.out.println("Ingrese el apellido del estudiante número "+(i+1)+": ");
+					DatosEst[i][j] = teclado.next();
+				}
+				if (j >= 2) {
+					System.out.println("Ingrese las notas de los exámenes realizados: ");
+					DatosEst[i][j] = teclado.nextDouble();
 				}
 			}
+		}
+
+		teclado.close();
+
+		double SumNotas=0;
+
+		// Double NotaMayor=0.0;
+		for (int i=0;i<DatosEst.length;i++) {
+			for ( int j=0;j<CantCol;j++) {
+				//System.out.println(DatosEst[i][j]);
+
+				if (j>=2 && j<CantCol-1) {
+					SumNotas=SumNotas+Double.parseDouble(DatosEst[i][j].toString());
+				}else if (j == CantCol-1) {
+					DatosEst[i][j]=SumNotas/TotExm;
+					SumNotas=0;
+				}
+				
+
+			}
+
+
+		}
+
+		int j = CantCol-1;
+		double auxiliar = 0;
+		String auxiliarNombre = "";
+		String auxiliarApellido = "";
+
+		for (int k = 1; k < DatosEst.length; k++) {
+
+			for (int i = 0; i < DatosEst.length-k; i++) {
+
+				if(Double.parseDouble(DatosEst[i][j].toString()) > Double.parseDouble(DatosEst[i+1][j].toString()) ) {
+					auxiliar = Double.parseDouble(DatosEst[i][j].toString());
+					auxiliarNombre = DatosEst[i][0].toString();
+					auxiliarApellido = DatosEst[i][1].toString();
+					DatosEst[i][j] = DatosEst[i+1][j];
+					DatosEst[i][0] = DatosEst[i+1][0];
+					DatosEst[i][1] = DatosEst[i+1][1];					
+					DatosEst[i+1][j] = auxiliar;
+					DatosEst[i+1][0] = auxiliarNombre;
+					DatosEst[i+1][1] = auxiliarApellido;
+
+
+
+
+
+				}
+
+			}
+		}
+
+		System.out.println("Mejor calificación: \nNombre: "+ DatosEst[TotEst-1][0]+ " \nApellido: "+ DatosEst[TotEst-1][1]+" \nPromedio Definitivo: "+DatosEst[TotEst-1][CantCol-1]);
+		System.out.println("==========================================");
+		System.out.println("Peor calificación: \nNombre: "+ DatosEst[0][0]+ " \nApellido: "+ DatosEst[0][1]+" \nPromedio Definitivo: "+DatosEst[0][CantCol-1]);
+		
+		
+		
+		System.out.println("==========================================");
+		System.out.println("GENERAL");
+		System.out.println("==========================================");
+		for (int i=0;i<DatosEst.length;i++) {
+			for ( int m=0;m<CantCol;m++) {
+					
+				
+			}
+			if (Double.parseDouble(DatosEst[i][CantCol-1].toString()) >= 7) {
+				System.out.print(DatosEst[i][0] + " " + DatosEst[i][1] + " ==> PROMOCIONADO " + " \nPromedio Definitivo: " + DatosEst[i][CantCol-1]);
+				System.out.println("\n==========================================");
+			}else {
+				System.out.print(DatosEst[i][0] + " " + DatosEst[i][1] + " ==> NO PROMOCIONADO " + " \nPromedio Definitivo" + DatosEst[i][CantCol-1]);
+				System.out.println("\n==========================================");
+			}
+			System.out.println("");
+		}
+
+
 
 	}
 
-
-		
-
+}
